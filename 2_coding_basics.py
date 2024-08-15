c4e0b4524e32c4c29c87c9b7fa18216dddf5e852
BandsThoBaby <32070633+BandsThoBaby@users.noreply.github.com>
2024-08-15 05:52:53 +0100

    Add files via upload

---
+++ 2_coding_basics.py
@@ -0,0 +1,26 @@
+## Coding Basics ##
+
+## Download Visual Studio Code - DONE ##
+
+# pip install #
+
+import pandas as pd
+import ccxt as c
+import numpy as np
+import datetime
+import ta
+import backtrader 
+
+
+## STUDY PANDAS & CCXT - 1 HOUR EVERYDAY ##
+
+## Important code to know - PYTHON 3 ##
+
+# variables
+# loops
+# functions
+
+# 4 Hours per Day - MINIMUM!!!!! #
+
+## The real ALPHA comes when BUILDING YOUR OWN stuff ##
+
